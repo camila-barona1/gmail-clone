2d5b1f38122dc0825a25f0b5810df3ff6578c26c
Maria CB <camilabarona1@gmail.com>
2021-05-14 13:18:22 -0500

    change

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-import firebase from 'firebase';
+import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCeEvsQOOEVSq8PiZwIgHvzZfen0hh-3ng",
-    authDomain: "clone-a564d.firebaseapp.com",
-    projectId: "clone-a564d",
-    storageBucket: "clone-a564d.appspot.com",
-    messagingSenderId: "646377412595",
-    appId: "1:646377412595:web:3b36e2b8a13384e0aee486",
-    measurementId: "G-ZVZY04HQN3"
-  };
+  apiKey: "AIzaSyCeEvsQOOEVSq8PiZwIgHvzZfen0hh-3ng",
+  authDomain: "clone-a564d.firebaseapp.com",
+  projectId: "clone-a564d",
+  storageBucket: "clone-a564d.appspot.com",
+  messagingSenderId: "646377412595",
+  appId: "1:646377412595:web:3b36e2b8a13384e0aee486",
+  measurementId: "G-ZVZY04HQN3",
+};
 
-  const firebaseApp = firebase.initializeApp(firebaseConfig)
-  const db = firebaseApp.firestore();
-  const auth = firebase.auth();
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
 
-  const provider = new firebase.auth.GoogleAuthProvider();
+const provider = new firebase.auth.GoogleAuthProvider();
 
-  export {db, auth, provider};
+export { db, auth, provider };
